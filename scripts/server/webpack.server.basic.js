45004139e074fd82796b78122acd91aed788e31c
frank.leo <1542874601@qq.com>
2023-06-29 10:56:44 +0800

    temp:临时节点标记 星期四 上午 10:56:44 六月 29日 2023 创建了 src/statics/swagger/favicon-16x16.png 创建了 src/statics/swagger/favicon-32x32.png 创建了 src/statics/swagger/index.css 创建了 src/statics/swagger/index.html 创建了 src/statics/swagger/oauth2-redirect.html 创建了 src/statics/swagger/swagger-initializer.js 创建了 src/statics/swagger/swagger-ui-bundle.js 创建了 src/statics/swagger/swagger-ui-bundle.js.map 创建了 src/statics/swagger/swagger-ui-es-bundle-core.js 创建了 src/statics/swagger/swagger-ui-es-bundle-core.js.map 创建了 src/statics/swagger/swagger-ui-es-bundle.js 创建了 src/statics/swagger/swagger-ui-es-bundle.js.map 创建了 src/statics/swagger/swagger-ui-standalone-preset.js 创建了 src/statics/swagger/swagger-ui-standalone-preset.js.map 创建了 src/statics/swagger/swagger-ui.css 创建了 src/statics/swagger/swagger-ui.css.map 创建了 src/statics/swagger/swagger-ui.js 创建了 src/statics/swagger/swagger-ui.js.map 修改了 package.json 修改了 scripts/server/webpack.server.basic.js 修改了 src/index.ts

--- scripts/server/webpack.server.basic.js
+++ scripts/server/webpack.server.basic.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const WebpackBar = require("webpackbar");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const program_loader = require("../configs/program_loader");
 
@@ -29,6 +30,12 @@ module.exports = {
     rules: [].concat(program_loader)
   },
   plugins: [
-    new WebpackBar({ name: "编译服务端" })
+    new WebpackBar({ name: "编译服务端" }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(process.cwd(), "./src/statics/"),
+        to: path.resolve(process.cwd(), "./dist/statics")
+      }]
+    })
   ]
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import express from "express";
 import bodyParser from "body-parser";
-import { DataSource } from "typeorm";
+// import { DataSource } from "typeorm";
 import cookieParser from "cookie-parser";
 import history_fallback from "connect-history-api-fallback";
 
@@ -12,12 +12,21 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 /** 这里开始放路由和api接口 **/
-
 // app.use(...);
 
-/** 这里开始提供前端渲染服务 **/
+/** SwaggerAPI文档 **/
+const filebase_dir = path.dirname(__filename);
+const swagger_file_dir = path.resolve(filebase_dir, "./statics/swagger/");
+app.use("/docs", express.static(swagger_file_dir));
+app.use("/docs/swagger.json", (request, response) => {
+  // response.send(swagger_config);
+});
+
+/** history_fallback **/
 app.use(history_fallback());
+/** 这里开始提供前端渲染服务 **/
 app.use(express.static(path.resolve(path.dirname(__filename), "./application/")));
+
 
 const server = app.listen(listen_port, "0.0.0.0", () => {
   console.log("address", server.address());
