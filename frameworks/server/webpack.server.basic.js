d8309d861e3cc9dd488c6bef570109fd5101cf72
frank.leo <1542874601@qq.com>
2023-12-20 21:25:51 +0800

    temp:临时节点标记 星期三 晚上 9:25:51 十二月 20日 2023 修改了 frameworks/server/webpack.server.basic.js

--- frameworks/server/webpack.server.basic.js
+++ frameworks/server/webpack.server.basic.js
@@ -5,11 +5,6 @@ const WebpackBar = require("webpackbar");
 const program_loader = require("../configs/program_loader");
 
 module.exports = {
-  cache: {
-    type: "filesystem",
-    memoryCacheUnaffected: true,
-    allowCollectingMemory: true,
-  },
   target: "node",
   devtool: "source-map",
   resolve: {
