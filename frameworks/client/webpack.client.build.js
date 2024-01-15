ab3475ce6d2a861d4ca78e2e5ad044ff94a8f110
frank.leo <1542874601@qq.com>
2024-01-15 15:38:48 +0800

    temp:临时节点标记 星期一 下午 3:38:48 一月 15日 2024 修改了 frameworks/client/webpack.client.build.js 修改了 frameworks/client/webpack.client.development.js

--- frameworks/client/webpack.client.build.js
+++ frameworks/client/webpack.client.build.js
@@ -6,7 +6,7 @@ const basic_client_config = require("../client/webpack.client.basic");
 module.exports = merge(basic_client_config, {
   mode: "production",
   output: {
-    clean: true,
+    clean: false,
     path: path.resolve(process.cwd(), "./dist/application/"),
     filename: "application.[fullhash].js",
   }
