56c5b7a2b3ccff48bdcd8920a6c02956465801e1
frankleo <1542874601@qq.com>
2026-03-22 15:54:07 +0800

    重新编排了loader

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 
 module.exports = {
-  presets: [
-    ["@babel/preset-env"],
-    ["@babel/preset-react"],
-    ["@babel/preset-typescript"]
-  ],
+  // presets: [
+  //   ["@babel/preset-env"],
+  //   ["@babel/preset-react"],
+  //   ["@babel/preset-typescript"]
+  // ],
   plugins: [
     ["@babel/plugin-transform-runtime"],
     ["import", {
--- frameworks/configs/program_loader.js
+++ frameworks/configs/program_loader.js
@@ -1,23 +1,23 @@
 const path = require("path");
 
 module.exports = [{
-  test: /\.(ts|tsx)$/,
+  test: /\.(js|jsx|mjs|cjs|ts|tsx)$/,
   exclude: /(node_modules)/,
   use: [{
-    loader: "ts-loader",
-    options: {
-      configFile: path.resolve(process.cwd(), "./tsconfig.json")
-    }
-  }]
-}, {
-  test: /\.(js|jsx)$/,
-  exclude: /(node_modules)/,
-  use: [{
+    loader: "esbuild-loader",
+  }, {
     loader: "babel-loader",
     options: {
-      cacheDirectory: true,
-      cacheCompression: false,
+      // cacheDirectory: true,
+      // cacheCompression: false,
       configFile: path.join(process.cwd(), "./.babelrc.js")
+    }
+  }, {
+    loader: "ts-loader",
+    options: {
+      // cacheDirectory: true,
+      // cacheCompression: false,
+      configFile: path.resolve(process.cwd(), "./tsconfig.json")
     }
   }]
 }];
